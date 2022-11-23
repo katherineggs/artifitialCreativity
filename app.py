e567824a7ff620c28f661ed0b64c480618103423
katherineggs <katherinegarcia@ufm.edu>
2022-11-23 08:17:43 -0600

    adds flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 #setting up the webpage service
 app = Flask(__name__)
@@ -6,8 +6,17 @@ app = Flask(__name__)
 #here you can add routes
 @app.route('/', methods =['GET', 'POST'])
 def index():
-    return render_template("index.html")
+  if request.method=='POST':
+    input = str(request.form['w3review'])
+    
+    song = input + " bien y tu" # generate(input)
+    
 
+
+    # call func
+    return render_template("index.html", song= song)
+  else:
+    return render_template('index.html', song="")
     
 if __name__ == "__main__":
   app.run(debug=True, host="0.0.0.0", port=8080)
